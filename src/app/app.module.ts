af76d52171ddb22024c4ad8bdd88aa6ef41cbc98
Alexey Grinuk <lesha.grinuk@gmail.com>
2023-07-20 18:56:41 +0300

    удаление с вспылвающим окном

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import {EmployeeService} from "./employee/employee.service";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/employee/employee.ts
@@ -0,0 +1,14 @@
+export interface Employee {
+  id: number;
+  name: string;
+  surname: string;
+  secondSurname: string;
+  beginning: Date;
+  dismissal: Date;
+  phoneNumber: string;
+  email: string;
+  positionId: number;
+
+}
+
+
